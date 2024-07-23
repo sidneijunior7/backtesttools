0e1805c7b319699e9ee57ebcbad7886449c946ff
Your Name~ <sidneijunior@icloud.com>
2024-07-23 13:18:26 -0300

    Primeira Versão Pré-beta

--- main_dashboard.py
+++ main_dashboard.py
@@ -44,7 +44,8 @@ def create_dash(df):
     if start_date <= end_date:
         filtered_df = df[(df['DATE'] >= pd.to_datetime(start_date)) & (df['DATE'] <= pd.to_datetime(end_date))]
         valor_inicial = filtered_df['BALANCE'].iloc[0]
-        st.line_chart(filtered_df.set_index('DATE')['BALANCE'] - valor_inicial)
+        #st.line_chart(filtered_df.set_index('DATE')['BALANCE'] - valor_inicial)
+        st.area_chart(filtered_df.set_index('DATE')['BALANCE'] - valor_inicial)
     else:
         st.error("Erro: A data de início deve ser menor ou igual à data de término.")
 
