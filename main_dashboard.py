416b5ae122fbc1cbf7c72d83a8d6a3f84d01ba6f
Your Name~ <sidneijunior@icloud.com>
2024-07-23 19:17:03 -0300

    Primeira Versão Pré-beta

--- main_dashboard.py
+++ main_dashboard.py
@@ -26,7 +26,7 @@ def calculate_metrics(df, start_date, end_date):
     # Calcular as métricas
     metrics = {
         "Deposito": filtered_df['BALANCE'].iloc[0],
-        "Lucro Bruto": filtered_df['BALANCE'].iloc[-1] - filtered_df['BALANCE'].iloc[0],
+        "Lucro Bruto": "R$ {:.2f}".format(filtered_df['BALANCE'].iloc[-1] - filtered_df['BALANCE'].iloc[0]),
         "Lucro Máximo": filtered_df['BALANCE'].max() - filtered_df['BALANCE'].iloc[0],
         "Drawdown Relativo": filtered_df['BALANCE'].min() - filtered_df['BALANCE'].iloc[0],
         "Drawdown Maximo": round(dd_max.max(), 2),
@@ -63,13 +63,13 @@ def create_dash(df):
         with col1:
             c = st.container(border=True)
             c.caption('RETORNO')
-            c.metric(label="Lucro: ", value="R$ {:.2f}".format(metrics['Lucro Bruto']))
-            c.metric(label="Lucro Max: ", value="R$ {:.2f}".format(metrics['Lucro Máximo']))
+            c.metric(label="Lucro: ", value=metrics['Lucro Bruto'])
+            c.metric(label="Lucro Max: ", value=metrics['Lucro Máximo'])
         with col2:
             c = st.container(border=True)
             c.caption('RISCO')
-            c.metric(label="Drawdown Médio: ", value="R$ {:.2f}".format(metrics['Drawdown Medio']))
-            c.metric(label="Drawdown Máximo: ", value="R$ {:.2f}".format(metrics['Drawdown Maximo']))
+            c.metric(label="Drawdown Médio: ", value=metrics['Drawdown Medio'])
+            c.metric(label="Drawdown Máximo: ", value=metrics['Drawdown Maximo'])
     with tab2:
         col1, col2 = st.columns(2)
         with col1:
