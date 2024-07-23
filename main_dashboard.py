5535535aa12284ffe397de6b29ebe1a5bce8d81d
Your Name~ <sidneijunior@icloud.com>
2024-07-23 20:07:28 -0300

    Primeira Versão Pré-beta

--- main_dashboard.py
+++ main_dashboard.py
@@ -25,11 +25,11 @@ def calculate_metrics(df, start_date, end_date):
     # Calcular as métricas
     metrics = {
         "Deposito": filtered_df['BALANCE'].iloc[0],
-        "Lucro Bruto": "R${:,.2f}".format(filtered_df['BALANCE'].iloc[-1] - filtered_df['BALANCE'].iloc[0]),
-        "Lucro Máximo": "R${:,.2f}".format(filtered_df['BALANCE'].max() - filtered_df['BALANCE'].iloc[0]),
-        "Drawdown Relativo": "R${:,.2f}".format(filtered_df['BALANCE'].min() - filtered_df['BALANCE'].iloc[0]),
-        "Drawdown Maximo": "R${:,.2f}".format(round(dd_max.max(), 2)),
-        "Drawdown Medio": "R${:,.2f}".format(round(dd_max.mean(), 2)),
+        "Lucro Bruto": filtered_df['BALANCE'].iloc[-1] - filtered_df['BALANCE'].iloc[0],
+        "Lucro Máximo": filtered_df['BALANCE'].max() - filtered_df['BALANCE'].iloc[0],
+        "Drawdown Relativo": filtered_df['BALANCE'].min() - filtered_df['BALANCE'].iloc[0],
+        "Drawdown Maximo": round(dd_max.max(), 2),
+        "Drawdown Medio": round(dd_max.mean(), 2),
         "Dias": filtered_df['DATE'].dt.date.nunique(),
         "Dias Positivos": positive_days['DATE_ONLY'].nunique()
     }
