348dbeea99abbf59ac147bfce6b66be3ef4b4804
Your Name~ <sidneijunior@icloud.com>
2024-07-24 08:39:22 -0300

    Primeira Versão Pré-beta

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ if authentication_status:
         st.logo('img/logo-white.webp')
 
     st.sidebar.write(f'Bem-vindo, {name} :smile: \n')
-    st.sidebar.write(st.get_option("base"))
+    st.sidebar.write(st.get_option("theme.base"))
     # Adicionar o botão de logout na sidebar
     authenticator.logout('Sair','sidebar',None)
 
